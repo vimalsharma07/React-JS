f821462fe69ffd17d300cf310951ce054834cd17
Vimal Sharma <99383911+vimalsharma07@users.noreply.github.com>
2023-02-27 11:37:52 +0530

    Add files via upload

---
+++ code-jsx001/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+      <div className= "maincontainer">
+        
+        <div className="sub"> 
+        <div   >  <h1> 
+            About Us
+          </h1></div>
+          <p>
+            At Zappos.com, our purpose is simple: to live and deliver WOW.
+
+Twenty years ago, we began as a small online retailer that only sold shoes. Today, we still sell shoes — as well as clothing, handbags, accessories, and more. That "more" is providing the very best customer service, customer experience, and company culture. We aim to inspire the world by showing it's possible to simultaneously deliver happiness to customers, employees, vendors, shareholders, and the community in a long-term, sustainable way.
+
+We hope that in the future people won't even realize we started selling shoes online. Instead, they'll know Zappos as a service company that just happens to sell ________.
+          </p>
+        </div>
+        
+      </div>
+  );
+}
+
+export default App;
