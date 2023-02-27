c01bad61974ed6483b06cd8608ddd5fab7ca0264
Vimal Sharma <99383911+vimalsharma07@users.noreply.github.com>
2023-02-27 13:37:33 +0530

    Add files via upload

---
+++ code-jsx-002/src/App.js
@@ -0,0 +1,19 @@
+import airbnbimage from './airbnbimage.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div  className="text">
+        <h1  className="first">  The Evolution of Airbnb's</h1>
+        <h1  className="second"> Airbnb's Frontend</h1>
+        <h1  className="third"> @spikebrehm </h1>
+         <img src=  {airbnbimage}    />
+      
+    </div>
+    </div>
+  );
+}
+
+export default App;
